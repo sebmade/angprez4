b9744851c872a2d63bfb1983e7511c67a974cc30
Sebastien Letelie <sebastien.letelie@gmail.com>
2014-01-04 21:36:19 +0100

    2

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('angprez4App')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope, $document) {
     $scope.slideIndex = 0;
 
     $scope.nextSlide = function () {
@@ -14,4 +14,17 @@ angular.module('angprez4App')
     $scope.currentSlide = function () {
       return "slide"+$scope.slideIndex+".html";
     };
+
+    $document.keydown(function(event) {
+      if (event.keyCode == 37) {
+        $scope.$apply(function() {
+          $scope.previousSlide();
+        });
+      } else if (event.keyCode == 39) {
+        $scope.$apply(function() {
+          $scope.nextSlide();
+        });
+      }
+    });
+
   });

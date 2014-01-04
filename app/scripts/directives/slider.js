3f88a4ed618bdf89581feeb6dc5bf546b5b24bc2
Sebastien Letelie <sebastien.letelie@gmail.com>
2014-01-04 21:50:14 +0100

    4

---
+++ app/scripts/directives/slider.js
@@ -0,0 +1,12 @@
+'use strict';
+
+angular.module('angprez4App')
+  .directive('slider', function () {
+    return {
+      templateUrl: 'views/main.html',
+      restrict: 'E',
+      link: function postLink(scope, element, attrs) {
+
+      }
+    };
+  });
